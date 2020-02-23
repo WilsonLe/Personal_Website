013863f8bb6877a4af5c21324f8d4eb865b6865a
WilsonLe <leanhminh2907@gmail.com>
2020-02-23 23:52:30 +0700

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var fs = require('fs');
+
+//create route
+app.use(express.static('.')) //allow client to browse whole folder
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/home.html");
+});
+
+//when a client connect, do
+io.on('connection', function (socket) {
+    console.log(socket.id + " has connected to the server");
+    socket.on('disconnect', function () {
+        console.log(socket.id + " has disconnected from the server");
+    })
+})
+
+http.listen(80);
